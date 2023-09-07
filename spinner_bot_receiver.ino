6526bc78ea9502ab9d273be9b58cff3cf9694f47
kc0hwi <kc0hwi@Hotmail.com>
2023-09-06 19:22:25 -0500

    Update spinner_bot_receiver.ino
    initial commit had wrong code

--- spinner_bot_receiver.ino
+++ spinner_bot_receiver.ino
@@ -1,81 +1,187 @@
-#include <SPI.h>      //the communication interface with the modem
-#include "RF24.h"     //the library which helps us to control the radio modem (nRF24L)
-
-//Motor A
-const int RightMotorForward = 2;    // IN1
-const int RightMotorBackward = 4;   // IN2
-
-//Motor B
-const int LeftMotorForward = 7;     // IN3
-const int LeftMotorBackward = 8;    // IN4
+#include <SPI.h>      //SPI library for communication with the nRF24L01+
+#include "RF24.h"  //The main library of the nRF24L01+
+#include "Wire.h"
 
 
-int data[2];
+RF24 radio (7, 8); // CE,CSN
 
-RF24 radio(10, 9); //10 and 9 are a digital pin numbers to which signals CE and CSN are connected
+//Create a pipe addresses for  communication
+const uint64_t pipe = 0xE8E8F0F0E1LL;
 
-const uint64_t pipe = 0xE8E8F0F0E1LL; //the address of the modem,that will receive data from the Arduino
+struct JoystickData {
+  int X;
+  int Y;
+  bool buttonState;
+} joystickData;
+
+//typedef struct package Package;
+//Package pos;
+
+int buttonState;
+int spinnerState = false ;
+int motora_1 = 3;
+int motora_2 = 4;
+int motorb_1 = 5;
+int motorb_2 = 6;
+int spinner = 2;
 
 
-void setup() {
+
+void setup()
+{
   Serial.begin(9600);
-  pinMode(RightMotorForward, OUTPUT);
-  pinMode(LeftMotorForward, OUTPUT);
-  pinMode(RightMotorBackward, OUTPUT);
-  pinMode(LeftMotorBackward, OUTPUT);
-
-  radio.begin();                    //it activates the modem
-  radio.openReadingPipe(1, pipe);   //determines the address of our modem which receive data
-  radio.startListening();           //enable receiving data via modem
+  radio.begin();                    //Start the nRF24 communicate
+  radio.openReadingPipe(1, pipe);   //Sets the address of the transmitter to which the program will receive data.
+  radio.startListening();
+  pinMode(motora_1, OUTPUT);
+  pinMode(motora_2, OUTPUT);
+  pinMode(motorb_1, OUTPUT);
+  pinMode(motorb_2, OUTPUT);
+  pinMode(spinner, OUTPUT);
+  digitalWrite(spinner, LOW);
+  delay(100);
 }
 
-void loop() {
+void loop()
+{
+
   if (radio.available()) {
-    radio.read(data, sizeof(data));
+    radio.read(&joystickData, sizeof(joystickData));
+    
+    // Process and use the received joystick data as needed
+    
+//    Serial.print("X:");
+//    Serial.println(pos.X);
 
-    //data X
-    if (data[0] > 550) {
-      digitalWrite(RightMotorForward, HIGH);
-      digitalWrite(RightMotorBackward, LOW);
-      digitalWrite(LeftMotorForward, HIGH);
-      digitalWrite(LeftMotorBackward, LOW);
-      Serial.println("FORWARD");
-    }
-
-    //data X
-    if (data[0] < 400) {
-      digitalWrite(RightMotorForward, LOW);
-      digitalWrite(RightMotorBackward, HIGH);
-      digitalWrite(LeftMotorForward, LOW);
-      digitalWrite(LeftMotorBackward, HIGH);
-      Serial.println("BACKWARD");
-    }
-
-    //data Y
-    if (data[1] > 550 ) {
-      digitalWrite(RightMotorForward, LOW);
-      digitalWrite(RightMotorBackward, HIGH);
-      digitalWrite(LeftMotorForward, HIGH);
-      digitalWrite(LeftMotorBackward, LOW);
-      Serial.println("TURN RIGHT");
-    }
-
-    //data Y
-    if (data[1] < 400 ) {
-      digitalWrite(RightMotorForward, HIGH);
-      digitalWrite(RightMotorBackward, LOW);
-      digitalWrite(LeftMotorForward, LOW);
-      digitalWrite(LeftMotorBackward, HIGH);
-      Serial.println("TURN LEFT");
-    }
-
-    if (data[0] < 550 && data[0] > 400 && data[1] < 550 && data[1] > 400) {
-      digitalWrite(RightMotorForward, LOW);
-      digitalWrite(RightMotorBackward, LOW);
-      digitalWrite(LeftMotorForward, LOW);
-      digitalWrite(LeftMotorBackward, LOW);
-      Serial.println("STOP");
-    }
-
+//    Serial.print("      buttonState");
+//    Serial.println(buttonState);
   }
+
+  int xAxis = joystickData.X;
+  int yAxis = joystickData.Y;
+  bool buttonState = joystickData.buttonState;
+//  Serial.print("X:");
+//  Serial.println(joystickData.X);
+  Serial.print("Y ");
+  Serial.println(joystickData.Y);
+//  int forward = map(yAxis, 570, 1024, 0, 255);
+//  int backward = map(yAxis, 480, 0, 0, 255);
+  // For some reason, the joystick hits 0 when all the way forward
+  int backward = map(yAxis, 570, 1024, 0, 255);
+  int forward = map(yAxis, 480, 1, 0, 255);
+  int right = map(xAxis, 570, 1024, 0, 255);
+  int left = map(xAxis, 480, 0, 0, 255);
+
+// Move forward
+  if (yAxis == 0) {
+    // Set Motor A stop
+    analogWrite(motora_1, 0);
+    analogWrite(motora_2, 0);
+    // Set Motor B stop
+    analogWrite(motorb_1, 0);
+    analogWrite(motorb_2, 0);
+  } else if (yAxis < 480) {                  // Move forward
+    // Set Motor A forward
+    analogWrite(motora_1, forward);
+    analogWrite(motora_2, 0);
+    // Set Motor B forward
+    analogWrite(motorb_1, forward);
+    analogWrite(motorb_2, 0);
+  } else if (yAxis > 550) {           // Move backwards
+    // Set Motor A backward
+    analogWrite(motora_1, 0);
+    analogWrite(motora_2, backward);
+    // Set Motor B backward
+    analogWrite(motorb_1, 0);
+    analogWrite(motorb_2, backward);    
+  } else if (xAxis > 570) {           // Move right
+    // Set Motor A forward
+    analogWrite(motora_1, right);
+    analogWrite(motora_2, 0);
+    // Set Motor B stop
+    analogWrite(motorb_1, 0);
+    analogWrite(motorb_2, 0); 
+  } else if (xAxis < 480 ){           // Move left
+    // Set Motor A stop
+    analogWrite(motora_1, 0);
+    analogWrite(motora_2, 0);
+    // Set Motor B foward
+    analogWrite(motorb_1, left);
+    analogWrite(motorb_2, 0);   
+  } else {
+    // Set Motor A stop
+    analogWrite(motora_1, 0);
+    analogWrite(motora_2, 0);
+    // Set Motor B stop
+    analogWrite(motorb_1, 0);
+    analogWrite(motorb_2, 0);    
+  }
+//  if (yAxis > 570) {          // Move Forward
+//    // Set Motor A forward
+//    Serial.print(pos.Y);
+//    Serial.println(" move forward");
+//    analogWrite(motora_1, forward);
+//    analogWrite(motora_2, 0);
+//    // Set Motor B forward
+//    analogWrite(motorb_1, forward);
+//    analogWrite(motorb_2, 0);
+//  } else if (yAxis < 480) {                     //Move Backward
+//    // Set Motor A backward
+//    analogWrite(motora_1, 0);
+//    analogWrite(motora_2, backward);
+//    // Set Motor B backward
+//    analogWrite(motorb_1, 0);
+//    analogWrite(motorb_2, backward);
+//  } else if (xAxis > 570) {                     // Move Right
+//    // Set Motor A forward
+//    analogWrite(motora_1, right);
+//    analogWrite(motora_2, 0);
+//    // Set Motor B stop
+//    analogWrite(motorb_1, 0);
+//    analogWrite(motorb_2, 0);
+//  } else if (xAxis < 480) {                     // Move Left
+//    // Set Motor A stop
+//    analogWrite(motora_1, 0);
+//    analogWrite(motora_2, 0);
+//    // Set Motor B foward
+//    analogWrite(motorb_1, left);
+//    analogWrite(motorb_2, 0);
+//  } else  {     //stop
+//    // Set Motor A stop
+//    analogWrite(motora_1, 0);
+//    analogWrite(motora_2, 0);
+//    // Set Motor B stop
+//    analogWrite(motorb_1, 0);
+//    analogWrite(motorb_2, 0);
+//  }
+    Serial.print("button state: ");
+    Serial.println(buttonState);
+    if (buttonState == 0 ) {
+      spinnerState = ! spinnerState;
+      delay(50);
+    }
+//
+    if (spinnerState == true) {
+      digitalWrite(spinner, HIGH);              // Turn On spinner
+      delay(50);
+    } else {
+      digitalWrite(spinner, LOW);               // Turn Off spinner
+      delay(50);
+    }
+
+// Testing
+// Forward
+    // Set Motor A forward
+    //analogWrite(motora_1, 255);
+    //analogWrite(motora_2, 0);
+    // Set Motor B forward
+    //analogWrite(motorb_1, 255);
+    //analogWrite(motorb_2, 0);
+// Right
+    // Set Motor A forward
+    //analogWrite(motora_1, right);
+    //analogWrite(motora_2, 0);
+    // Set Motor B stop
+    //analogWrite(motorb_1, 0);
+    //analogWrite(motorb_2, 0);
 }
